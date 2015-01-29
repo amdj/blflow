51637605b7bd4912d078919febf740b807b65de1
Anne de Jong <anne@amdj.nl>
2015-01-29 07:36:01 +0100

    Parallel plate flow also working

--- parplateflow.py
+++ parplateflow.py
@@ -8,6 +8,8 @@ matplotlib.use('TkAgg')
 # from matplotlib.pylab import *
 import pylab as p
 
+
+
 # import matplotlib.animation as animation
 def K(t):                                 #Forcing function
     return (1-exp(-0.1*t))*cos(t)
@@ -16,16 +18,16 @@ def K(t):                                 #Forcing function
 s=10
 #Define domain
 n=50                                      #Number of gridpoints
-y=linspace(0,1,n)
+y=linspace(-1,1,n)
 
 dy=y[1]-y[0]
 dt=0.0005
 
 l=(dt/(s**2*dy**2))
 
-hnu=exp(-sqrt(1j)*s*y)
-# fnu=(1-1j)/s
-fnu=0
+hnu=cosh(sqrt(1j)*s*y)/cosh(sqrt(1j)*s)
+fnu=tanh(sqrt(1j)*s)/(sqrt(1j)*s)
+
 
 def u_ex(tn):
     return (((1-hnu)/(1-fnu))*exp(1j*(tn))/1j).real
@@ -34,9 +36,9 @@ def u_ex(tn):
 def u_np1(un,tn,dt):
     Kn=K(tn)
     unp1=un
-    unp1[0]=0                             #Velocity zero ver here
+    unp1[0]=0                                  #Velocity zero ver here
     unp1[1:-1]=un[1:-1]+dt*Kn+l*(un[0:-2]-2*un[1:-1]+un[2:])
-    unp1[-1]=unp1[-2]                     #Approximate 'infinity' bc
+    unp1[-1]=0                     #Boundary other side
     return unp1
 
 un0=zeros(n,float)
@@ -49,7 +51,7 @@ p.ion()
 linefd, = p.plot(un0,y)
 linee, = p.plot(un0,y)
 p.legend(('Finite difference','Periodic exact'))
-p.ylim(0,1)
+p.ylim(-1,1)
 p.xlim(-1.5,1.5)
 p.ylabel('y')
 p.xlabel('u')
